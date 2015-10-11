f5f3e29c586553077d13dc7e17b123c71284c167
sxgolst <sxgolst@gmail.com>
2015-10-11 06:26:45 -0700

    Revert "Revert "Project 4 Guest Comment Page""
    This reverts commit 4ace0b13ec75f197884afd63cabec02c3d21c38a.

---
+++ My Templates/myform.py
@@ -0,0 +1,9 @@
+<html>
+<form>
+<h2>User Information and Comments</h2>
+<input type="text" value="name">
+<input type="text" value="email">
+<div><textarea name="content" rows="3" cols="60"></textarea></div> 
+<div><input type="submit" value="Post Comments"></div>
+</form>
+</html>
